a88c2c511178b7a801570fece8160b0bfcbd7ad5
JatinAggarwal04 <jaggarwal1_be22@thapar.edu>
2025-10-19 17:09:10 +0530

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -169,7 +169,7 @@ app = FastAPI(lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware, 
-    allow_origins=["http://localhost:3000", "http://localhost:5173","https://recommendation-system-topaz.vercel.app/"], 
+    allow_origins=["http://localhost:3000", "http://localhost:5173","https://recommendation-system-topaz.vercel.app"], 
 
     allow_credentials=True, 
     allow_methods=["*"], 
